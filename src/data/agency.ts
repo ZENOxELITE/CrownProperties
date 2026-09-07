5630a843cc235b18c3950f137a900e339f3c3825
Muneer <muneerahmed12shaikh@gmail.com>
2026-09-07 20:07:59 +0500

    Added social links

--- src/data/agency.ts
+++ src/data/agency.ts
@@ -16,7 +16,7 @@ export const agency = {
   phoneDisplay: '+92 21 3537 8820',
   whatsapp: '922135378820',
   whatsappDisplay: '+92 21 3537 8820',
-  email: 'info@sahilsaaya.pk',
+  email: 'info@crownproperties.pk',
   address: {
     line1: 'Sunset Boulevard, Block 2',
     line2: 'Clifton, Karachi 75600',
@@ -25,9 +25,9 @@ export const agency = {
   mapsEmbedOffice:
     'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3626.2!2d67.0291!3d24.8137!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjTCsDQ4JzQ5LjMiTiA2N8KwMDEnNDQuOCJF!5e0!3m2!1sen!2s!4v1700000000000',
   social: {
-    instagram: 'https://instagram.com/sahilsaaya',
-    facebook: 'https://facebook.com/sahilsaaya',
-    linkedin: 'https://linkedin.com/company/sahilsaaya',
+    instagram: 'https://instagram.com/crownproperties',
+    facebook: 'https://facebook.com/crownproperties',
+    linkedin: 'https://linkedin.com/company/crownproperties',
   },
   license: 'SREAR-2024-1187',
   stats: {
